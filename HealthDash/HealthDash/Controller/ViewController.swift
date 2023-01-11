9aa89f142cc4966b7235f53fe8ff07dd68dd76ed
Caroline Frey <cfrey9@gmail.com>
2023-01-11 15:35:23 -0700

    Remove storyboard

---
+++ HealthDash/HealthDash/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardViewController.swift
+//  HealthDash
+//
+//  Created by Caroline Frey on 1/11/23.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
